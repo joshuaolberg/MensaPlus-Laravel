473eba780bcc3eb061db7c300f2f0bd84e4a3206
joshua.olberg <joshua.olberg@icloud.com>
2020-11-06 14:12:32 +0100

    add Meal to api

---
+++ backend/app/Http/Controllers/MealController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Meal;
+
+class MealController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // get all meals
+        return Meal::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // validate the data first
+        $request->validate([
+            'name' => 'required'
+        ]);
+
+        // create a meal
+        return Meal::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // show a meal
+        return Meal::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // update a meal
+        $meal = Meal::find($id);
+        $meal->update($request->all());
+        return $meal;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // delete
+        return Meal::destroy($id);
+    }
+}
